9a1567fdb8b7d0896b159e1c1303e88fd7d794c2
kyrylo_kundik <goliathusua@gmail.com>
2019-03-18 16:32:01 +0200

    deleted unused import

--- src/main/java/app/jdbcJPA/data/dao/impl/CustomerService.java
+++ src/main/java/app/jdbcJPA/data/dao/impl/CustomerService.java
@@ -10,7 +10,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import java.util.List;
-import java.util.logging.Logger;
 
 @Repository
 @Transactional
